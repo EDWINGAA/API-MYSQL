212281355d767782093362b9a37bc8357de69dd4
EDWINGAA <159578458+EDWINGAA@users.noreply.github.com>
2024-07-07 20:05:01 -0700

    GetInvitados

--- routes/invitados.js
+++ routes/invitados.js
@@ -2,12 +2,33 @@ const express = require("express");
 const Router = express.Router();
 const mysqlConexion = require("../conexion");
 
+// Endpoint para obtener invitados filtrados por nombreU
 Router.get("/", (req, res) => {
     mysqlConexion.query("SELECT * from invitados", (err, rows, fields) => {
         if (!err) {
             res.send(rows);
         } else {
             console.log(err);
+            res.status(500).send("Error en la consulta");
+        }
+    });
+});
+
+Router.get("/:nombreU", (req, res) => {
+    const nombreU = req.params.nombreU;
+    const query = `
+        SELECT invitados.*
+        FROM invitados
+        JOIN usuario ON invitados.nombreU = usuario.nombre
+        WHERE usuario.nombre = ?
+    `;
+
+    mysqlConexion.query(query, [nombreU], (err, rows, fields) => {
+        if (!err) {
+            res.send(rows);
+        } else {
+            console.log(err);
+            res.status(500).send("Error en la consulta");
         }
     });
 });
