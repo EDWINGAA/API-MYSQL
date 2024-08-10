bd39f14ff5ecc4cdaeeb07e7aae9eae560410dc4
EDWINGAA <159578458+EDWINGAA@users.noreply.github.com>
2024-08-09 23:45:49 -0700

    se agrego un get para mostrar los nombres de los usuarios y no el id en la tabla apertura

--- routes/apertura.js
+++ routes/apertura.js
@@ -2,16 +2,33 @@ const express = require("express");
 const Router = express.Router();
 const mysqlConexion = require("../conexion");
 
+// Endpoint para seleccionar todo de la tabla 'apertura'
 Router.get("/", (req, res) => {
-    mysqlConexion.query("SELECT * from apertura", (err, rows, fields) => {
+    mysqlConexion.query("SELECT * FROM apertura", (err, rows, fields) => {
         if (!err) {
+            console.log("Data fetched from / endpoint:", rows);
             res.send(rows);
         } else {
-            console.log(err);
+            console.log("Error fetching data:", err);
+            res.status(500).send("Error in the query");
         }
     });
 });
 
+Router.get("/withUserNames", (req, res) => {
+    const query = `
+        SELECT a.idapertura, a.fecha, a.hora, u.nombre 
+        FROM apertura a 
+        JOIN usuario u ON a.idusuario = u.idusuario
+    `;
 
+    mysqlConexion.query(query, (err, rows, fields) => {
+        if (!err) {
+            res.send(rows);
+        } else {
+            res.status(500).send("Error in the query");
+        }
+    });
+});
 
 module.exports = Router;
