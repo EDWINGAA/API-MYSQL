6d42c24d7e9b930b47031da5869ba196f6a372f2
EDWINGAA <159578458+EDWINGAA@users.noreply.github.com>
2024-08-11 20:21:15 -0700

    update-motor

--- routes/puertaS.js
+++ routes/puertaS.js
@@ -12,4 +12,20 @@ Router.get("/", (req, res) => {
     });
 });
 
+// Ruta para actualizar solo el campo "motor"
+Router.put("/update-motor", (req, res) => {
+    const { id, motor } = req.body;
+
+    const query = "UPDATE puertaS SET motor = ? WHERE idstatus = ?";
+
+    mysqlConexion.query(query, [motor, id], (err, result) => {
+        if (!err) {
+            res.send("Motor actualizado correctamente");
+        } else {
+            console.log(err);
+            res.status(500).send("Error actualizando el motor");
+        }
+    });
+});
+
 module.exports = Router;
